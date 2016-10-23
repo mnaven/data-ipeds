3f0c9a98038407cc080c73f99001a55ad5798677
Matthew Naven <msnaven@ucdavis.edu>
2016-10-22 18:19:24 -0700

    Edited IPEDS insheet error files
    Edited the Python files that make a list of all the do files that
    encountered an error while creating a dta file from the csv file
    downloaded from the IPEDS website. Added a shebang and header. Changed
    the location of the IPEDS directory. Renamed the log contains error
    function to log error function.

--- data_scrape/insheet_error_files.py
+++ data_scrape/insheet_error_files.py
@@ -1,21 +1,30 @@
+#!/usr/bin/env python
+
+"""Creates a list of do files that encountered an error when attempting to create a dta file from a csv file"""
+
 import os
-from log_contains_error_function import ipeds_log_contains_error
+from log_error_function import ipeds_log_error
 
-__author__ = 'Naven'
+__author__ = "Matthew Naven"
+__copyright__ = "Copyright 2016, Matthew Naven"
+__email__ = "msnaven@ucdavis.edu"
 
 
-directory = "/Users/Naven/Documents/Research/data/ipeds/raw_data"
+ipeds_directory = "/Users/Naven/Documents/Research/data/ipeds"
+log_files_directory = os.path.join(ipeds_directory, "log_files")
 error_files = list()
-for directory_path, directory_names_list, file_names_list in os.walk(directory):
+
+for directory_path, directory_names_list, file_names_list in os.walk(log_files_directory):
     for file_name in file_names_list:
-        print("Checking if " + file_name + " ends in .log")
+        print("Checking if " + file_name + " is a log file")
         if file_name.endswith(".log"):
-            print(file_name + " ends in .log")
+            print(file_name + " is a log file")
             print("Checking for errors in log file " + file_name)
             file_path = os.path.join(directory_path, file_name)
-            if ipeds_log_contains_error(file_path):
+            if ipeds_log_error(file_path):
                 print("There is an error in log file " + file_name)
                 error_files.append(file_name)
+
 print("The following files encountered an error and could not be insheeted", error_files)
 for file_name in error_files:
     print("Error running " + file_name)
--- data_scrape/log_error_function.py
+++ data_scrape/log_error_function.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python
+
+"""Checks if the do file that insheets the IPEDS csv file to a dta file has any errors in the log file"""
+
 import os
 import re
 
-__author__ = 'Naven'
+__author__ = "Matthew Naven"
+__copyright__ = "Copyright 2016, Matthew Naven"
+__email__ = "msnaven@ucdavis.edu"
 
 
-def ipeds_log_contains_error(file_path):
+def ipeds_log_error(file_path):
     file_name = os.path.basename(file_path)
+
     with open(file_path, 'r', encoding='iso-8859-1') as log_file:
         log_file_text = log_file.read()
         error_code_search = re.search("r\([0-9]{1,4}\);", log_file_text)
+
         if error_code_search:
             if error_code_search.group() == "r(602);":
-                print(file_name + " has already created a .dta file")
+                print(file_name + " has already been used to create a .dta file")
                 return False
+
             else:
                 print("There is an error in log file " + file_name)
                 return True
+
         else:
             print("No errors in log file " + file_name)
             return False
