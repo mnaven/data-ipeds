4d9396cc0509f0233ebe8a325988e2bdc2c72c15
Matthew Naven <msnaven@ucdavis.edu>
2016-10-22 18:23:28 -0700

    Edited IPEDS delete extracted files
    Edited the Python file that deletes the extracted do and csv files that
    were used to create dta files. Added a shebang and header. Changed the
    location of the IPEDS directory. Now keeping the html files since they
    are codebooks.

--- data_scrape/delete_extracted_files.py
+++ data_scrape/delete_extracted_files.py
@@ -1,27 +1,42 @@
+#!/usr/bin/env python
+
+"""Delete all IPEDS do, csv, and html files after the dta files have been created"""
+
 import os
 
-__author__ = 'Naven'
+__author__ = "Matthew Naven"
+__copyright__ = "Copyright 2016, Matthew Naven"
+__email__ = "msnaven@ucdavis.edu"
 
 
-directory = "/Users/Naven/Documents/Research/data/ipeds/raw_data"
+ipeds_directory = "/Users/Naven/Documents/Research/data/ipeds"
+
 # Go through all the files and folders contained in the directory, now including the created dta files
-for directory_path, directory_names_list, file_names_list in os.walk(directory):
+for directory_path, directory_names_list, file_names_list in os.walk(ipeds_directory):
     for file_name in file_names_list:
         file_path = os.path.join(directory_path, file_name)
         # Check to see if the file came from a zipped file
-        print("Checking if " + file_name + " ends in .do, .csv, or .html")
-        if file_name.endswith(".do"):
-            print(file_name + " ends in .do")
-            # Delete files that were unzipped because the dta file has now been created
+        print("Checking if " + file_name + " is a do, csv, or html file")
+
+        if file_name.endswith(".dta"):
+            print(file_name + " is a dta file")
+            print("Keeping " + file_name)
+
+        elif file_name.endswith(".log"):
+            print(file_name + " is a log file")
+            print("Keeping " + file_name)
+
+        elif file_name.endswith(".do"):
+            print(file_name + " is a do file")
             print("Deleting " + file_name)
             os.remove(file_path)
-        if file_name.endswith(".csv"):
-            print(file_name + " ends in .csv")
-            # Delete files that were unzipped because the dta file has now been created
+
+        elif file_name.endswith(".csv"):
+            print(file_name + " is a csv file")
             print("Deleting " + file_name)
             os.remove(file_path)
-        if file_name.endswith(".html"):
-            print(file_name + " ends in .html")
-            # Delete files that were unzipped because the dta file has now been created
-            print("Deleting " + file_name)
-            os.remove(file_path)
+
+        # elif file_name.endswith(".html"):
+        #     print(file_name + " is an html file")
+        #     print("Deleting " + file_name)
+        #     os.remove(file_path)
