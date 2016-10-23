7226cd36e05e56857a7c6640931485cc22ac3e14
Matthew Naven <msnaven@ucdavis.edu>
2016-10-22 17:28:16 -0700

    Edited IPEDS delete extras
    Edited the python files that delete the duplicate, SAS, and SPSS files
    downloaded from the IPEDS website. Added a shebang and header and
    changed the location of the IPEDS data directory.

--- data_scrape/delete_extras.py
+++ data_scrape/delete_extras.py
@@ -1,12 +1,19 @@
+#!/usr/bin/env python
+
+"""Deletes duplicate, SPSS, and SAS files downloaded from IPEDS"""
+
 import os
-import re
 from delete_extras_function import ipeds_delete_extras
 
-__author__ = 'Naven'
+__author__ = "Matthew Naven"
+__copyright__ = "Copyright 2016, Matthew Naven"
+__email__ = "msnaven@ucdavis.edu"
 
 
-directory = "/Users/Naven/Documents/Research/data/ipeds/raw_data"
-for directory_path, directory_names_list, file_names_list in os.walk(directory):
+ipeds_directory = "/Users/Naven/Documents/Research/data/ipeds"
+downloads_directory = os.path.join(ipeds_directory, "downloads")
+
+for directory_path, directory_names_list, file_names_list in os.walk(downloads_directory):
     for file_name in file_names_list:
         print("Checking if " + file_name + " is a duplicate, SPSS, or SAS file")
         file_path = os.path.join(directory_path, file_name)
--- data_scrape/delete_extras_function.py
+++ data_scrape/delete_extras_function.py
@@ -1,22 +1,32 @@
+#!/usr/bin/env python
+
+"""Creates a function that deletes duplicate, SPSS, and SAS files downloaded from IPEDS"""
+
 import os
 import re
 
-__author__ = 'Naven'
+__author__ = "Matthew Naven"
+__copyright__ = "Copyright 2016, Matthew Naven"
+__email__ = "msnaven@ucdavis.edu"
 
 
 def ipeds_delete_extras(file_path):
     file_name = os.path.basename(file_path)
+
     if re.search(r"\([0-9]+?\)", file_name) is not None:
         print(file_name + " is a duplicate")
         print("Deleting " + file_name)
         os.remove(file_path)
+
     elif os.path.splitext(file_name)[0].endswith("SPS"):
         print(file_name + " is a SPSS file")
         print("Deleting " + file_name)
         os.remove(file_path)
+
     elif os.path.splitext(file_name)[0].endswith("SAS"):
         print(file_name + " is a SAS file")
         print("Deleting " + file_name)
         os.remove(file_path)
+
     else:
         print(file_name + " is not a duplicate, SPSS, or SAS file")
