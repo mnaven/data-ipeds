b6f000adccff732d4b0b55f09da532bb06134dd6
Matthew Naven <msnaven@ucdavis.edu>
2016-10-23 17:29:24 -0700

    Added Linux support
    Added Linux support for the creation of dta files by checking the
    operating system and searching for the default Stata path. Still
    working on adding Windows support.

--- data_scrape/create_dta.py
+++ data_scrape/create_dta.py
@@ -3,6 +3,7 @@
 """Creates a dta file from an unzipped IPEDS csv file"""
 
 import os
+import sys
 from create_dta_function import ipeds_create_dta
 
 __author__ = "Matthew Naven"
@@ -10,9 +11,47 @@ __copyright__ = "Copyright 2016, Matthew Naven"
 __email__ = "msnaven@ucdavis.edu"
 
 
-do_files_directory = "/Users/Naven/Documents/Research/data/ipeds/do_files"
-stata_console_executable_path = "/Applications/Stata/StataMP.app/Contents/MacOS/Stata-mp"
-stata_gui_executable_path = "/Applications/Stata/StataMP.app/Contents/MacOS/StataMP"
+ipeds_directory = "/Users/Naven/Documents/Research/data/ipeds"
+do_files_directory = os.path.join(ipeds_directory, "do_files")
+
+os_platform = sys.platform
+
+if os_platform == "darwin":
+    operating_system = "Mac"
+    stata_directory = "/Applications/Stata"
+    if os.path.exists(os.path.join(stata_directory, "StataMP.app")):
+        stata_flavor = "MP"
+        stata_console_executable_path = "/Applications/Stata/StataMP.app/Contents/MacOS/stata-mp"
+        stata_gui_executable_path = "/Applications/Stata/StataMP.app/Contents/MacOS/StataMP"
+    elif os.path.exists(os.path.join(stata_directory, "StataSE.app")):
+        stata_flavor = "SE"
+        stata_console_executable_path = "/Applications/Stata/StataSE.app/Contents/MacOS/stata-se"
+        stata_gui_executable_path = "/Applications/Stata/StataSE.app/Contents/MacOS/StataSE"
+    elif os.path.exists(os.path.join(stata_directory, "Stata.app")):
+        stata_flavor = "IC"
+        stata_gui_executable_path = "/Applications/Stata/Stata.app/Contents/MacOS/Stata"
+    elif os.path.exists(os.path.join(stata_directory, "smStata.app")):
+        stata_flavor = "Small"
+        stata_gui_executable_path = "/Applications/Stata/smStata.app/Contents/MacOS/smStata"
+
+elif os_platform == "win32" or os_platform == "cygwin":
+    operating_system = "Windows"
+
+elif os_platform.startswith("linux"):
+    operating_system = "Linux"
+    stata_directory = "/user/local/bin"
+    if os.path.exists(os.path.join(stata_directory, "stata-mp")):
+        stata_flavor = "MP"
+        stata_console_executable_path = os.path.join(stata_directory, "stata-mp")
+        stata_gui_executable_path = os.path.join(stata_directory, "xstata-mp")
+    elif os.path.exists(os.path.join(stata_directory, "stata-se")):
+        stata_flavor = "SE"
+        stata_console_executable_path = os.path.join(stata_directory, "stata-se")
+        stata_gui_executable_path = os.path.join(stata_directory, "xstata-se")
+    elif os.path.exists(os.path.join(stata_directory, "stata")):
+        stata_flavor = "IC"
+        stata_console_executable_path = os.path.join(stata_directory, "stata")
+        stata_gui_executable_path = os.path.join(stata_directory, "xstata")
 
 # Go through all the files and folders contained in the directory, now including the unzipped files
 for directory_path, directory_names_list, file_names_list in os.walk(do_files_directory):
@@ -22,4 +61,7 @@ for directory_path, directory_names_list, file_names_list in os.walk(do_files_di
             print(file_name + " is a do file")
             print("Creating a dta file with Stata using the do file " + file_name)
             do_file_path = os.path.join(directory_path, file_name)
-            ipeds_create_dta(do_file_path, stata_console_executable_path)
+            if stata_console_executable_path is not None:
+                ipeds_create_dta(do_file_path, stata_console_executable_path)
+            else:
+                ipeds_create_dta(do_file_path, stata_gui_executable_path)
